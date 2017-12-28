993b3b19e89688fc3885b376d58a772a984329f7
Meg Ashby <megashby@utexas.edu>
2017-12-27 18:31:04 -0600

    Add files via upload

--- DRPother3.R
+++ DRPother3.R
@@ -22,7 +22,7 @@ tangentdiff <-function(x1, slope1, x2, slope2 ,x){
 
 temp<-seq(0,1,.1)
 
-mat <-matrix(, nrow = 11, ncol = 7)
+mat <-matrix(, nrow = 11, ncol = 8)
 
 
 for (n in 1:11){
@@ -68,6 +68,8 @@ integralval3 <-function(x2, x1, x0, slope){
 
 mat[1,6] = 0
 mat[1,7] = 0
+mat[1,8] = 0
+
 for (n in 2:11){
 	sumbefore <-0
 	for (i in n-1){
@@ -76,12 +78,15 @@ for (n in 2:11){
 	curintegral <- integralval3(mat[n,4], mat[n-1,4], mat[n-1,1], mat[n-1,3])
 	mat[n,6] <- curintegral
 	mat[n,7] <- curintegral + mat[n-1,7]
+	mat[n,8] <-mat[n,7]/1.005
+	
 	
 }
 
 
 lastsection <-integralval3(1, .95, 1, -6)
-lastsection
+lastsection/1.005 + mat[11,8]
+lastsection + .9975
 
 totalarea <- 1.005
 
@@ -94,6 +99,7 @@ envelope <-function(x){
 	ifelse(x<=.65, a<- tangent(.6, -1.2, x), ifelse(x<=.75, a<-tangent(.7, -2.4,x),
 	ifelse(x<=.85, a<-tangent(.8, -3.6, x), ifelse(x<=.95, a<-tangent(.9, -4.8,x),
 	a<- tangent(1, -6,x)))))))))))
+	return(a)
 	
 }
 
@@ -103,13 +109,14 @@ points(x0, envelopey)
 
 cdf <-function(x){
  a <- 0
-	ifelse(x <= .05, a <- integralval3(x,0, 0,6), ifelse (x<=.15, a <- integralval3(x,.05,.1,4.8)
-	+.0075, ifelse ( x<= .25, a<-integralval3(x, .15,.2, 3.6)+0.0615, ifelse(x<=.35, a<-integralval3(x, .25, .3, 2.4
-	)+.1575, ifelse(x<=.45, a<-integralval3(x,.35,.4, 1.2)+.2835, ifelse(x<=.55, a<-integralval3(x,.45,.5, 0)
-	+.4275,ifelse(x<=.65, a<- integralval3(x, .55, .6, -1.2)+.5775, ifelse(x<=.75, a<-
-	integralval3(x, .65,.7, -2.4)+.7215, ifelse(x<=.85, a<-integralval3(x, .75, .8, -3.6)+.8475,
-	 ifelse(x<=.95, a<-integralval3(x, .85,.9, -4.8)+.9435,
-	a<- integralval3(x, .95, 1, -6) + .9975))))))))))
+	ifelse(x <= .05, a <- integralval3(x,0, 0,6) + mat[1,8], ifelse (x<=.15, a <- integralval3(x,.05,.1,4.8)
+	+.mat[2,8], ifelse ( x<= .25, a<-integralval3(x, .15,.2, 3.6)+mat[3,8], ifelse(x<=.35, a<-integralval3(x, .25, .3, 2.4
+	)+mat[4,8], ifelse(x<=.45, a<-integralval3(x,.35,.4, 1.2)+mat[5,8], ifelse(x<=.55, a<-integralval3(x,.45,.5, 0)
+	+mat[6,8],ifelse(x<=.65, a<- integralval3(x, .55, .6, -1.2)+mat[7,8], ifelse(x<=.75, a<-
+	integralval3(x, .65,.7, -2.4)+mat[8,8], ifelse(x<=.85, a<-integralval3(x, .75, .8, -3.6)+mat[9,8],
+	 ifelse(x<=.95, a<-integralval3(x, .85,.9, -4.8)+mat[10,8],
+	a<- integralval3(x, .95, 1, -6) + mat[11,8]))))))))))
+	return(a)
 }
 #plot.new()
 #plot(1, type="n", xlab="", ylab="", xlim=c(0, 1), ylim=c(0, 1), main = 'CDF of Envelope')
@@ -141,26 +148,9 @@ plotrealx <-function(x){
 
 #points(plotrealx(.04), envelope(plotrealx(.04)), col = 'blue')
 #points(plotrealx(.34), envelope(plotrealx(.34)), col = 'blue')
+#points(plotrealx(.8), envelope(plotrealx(.8)), col = 'green')
 
 
-
-#DOES NOT WORK
-inversecdf<-function(x){
-	a<-0
-	if(x<=0.0075){
-		newx = x-0
-		a <-invcdfcalc(mat[1,3], mat[1,2], mat[1,1], mat[1,4],x)
-		}
-	else{
-	 	a<-0
-	} 
-	return(a)
-}
-
-inversecdf(.006)
-#points(.03162278, .006, col = 'purple')
-
-#points(x0, 4.8*x0 +.06, col = 'green')
 
 #invcdfcalc <-function(a, b, c, d, x1){
 #	(quadform(a/2, (b-c*a), (-x1+(a/2)*(d^2) +(b-c*a)*d)))
@@ -181,21 +171,32 @@ specialcase<-function(newx){
 
 invcdf<-function(x){
 	a<-0
-	ifelse(x<=0.0075, a<-invcdfcalc2(mat[1,3], mat[1,2], mat[1,1], mat[1,4], x-mat[1,7]), 
-	ifelse(x<=0.0615, a<-invcdfcalc2(mat[2,3], mat[2,2], mat[2,1], mat[2,4], x-mat[2,7]),
-	ifelse(x<=0.1575, a<-invcdfcalc2(mat[3,3], mat[3,2], mat[3,1], mat[3,4], x-mat[3,7]),
-	ifelse(x<=.2835, a<-invcdfcalc2(mat[4,3], mat[4,2], mat[4,1], mat[4,4], x-mat[4,7]),
-	ifelse(x<=.4275, a<-invcdfcalc2(mat[5,3], mat[5,2], mat[5,1], mat[5,4], x-mat[5,7]),
-	ifelse(x<=.5775, a<-specialcase(x-mat[6,7]),
-	ifelse(x<=.7215, a<-invcdfcalc2(mat[7,3], mat[7,2], mat[7,1], mat[7,4], x-mat[7,7]),
-	ifelse(x<=.8475, a<-invcdfcalc2(mat[8,3], mat[8,2], mat[8,1], mat[8,4], x-mat[8,7]),
-	ifelse(x<=.9435, a<-invcdfcalc2(mat[9,3], mat[9,2], mat[9,1], mat[9,4], x-mat[9,7]),
-	ifelse(x<=.9975, a<-invcdfcalc2(mat[10,3], mat[10,2], mat[10,1], mat[10,4], x-mat[10,7]),
-	 a<-invcdfcalc2(mat[11,3], mat[11,2], mat[11,1], mat[11,4], x-mat[11,7]) ))))))))))
+	ifelse(x<=mat[2,8], a<-invcdfcalc2(mat[1,3], mat[1,2], mat[1,1], mat[1,4], x-mat[1,8]), 
+	ifelse(x<=mat[3,8], a<-invcdfcalc2(mat[2,3], mat[2,2], mat[2,1], mat[2,4], x-mat[2,8]),
+	ifelse(x<=mat[4,8], a<-invcdfcalc2(mat[3,3], mat[3,2], mat[3,1], mat[3,4], x-mat[3,8]),
+	ifelse(x<=mat[5,8], a<-invcdfcalc2(mat[4,3], mat[4,2], mat[4,1], mat[4,4], x-mat[4,8]),
+	ifelse(x<=mat[6,8], a<-invcdfcalc2(mat[5,3], mat[5,2], mat[5,1], mat[5,4], x-mat[5,8]),
+	ifelse(x<=mat[7,8], a<-specialcase(x-mat[6,8]),
+	ifelse(x<=mat[8,8], a<-invcdfcalc2(mat[7,3], mat[7,2], mat[7,1], mat[7,4], x-mat[7,8]),
+	ifelse(x<=mat[9,8], a<-invcdfcalc2(mat[8,3], mat[8,2], mat[8,1], mat[8,4], x-mat[8,8]),
+	ifelse(x<=mat[10,8], a<-invcdfcalc2(mat[9,3], mat[9,2], mat[9,1], mat[9,4], x-mat[9,8]),
+	ifelse(x<=mat[11,8], a<-invcdfcalc2(mat[10,3], mat[10,2], mat[10,1], mat[10,4], x-mat[10,8]),
+	 a<-invcdfcalc2(mat[11,3], mat[11,2], mat[11,1], mat[11,4], x-mat[11,8]) ))))))))))
 }
 
-points(x0, 3.6*x0 + .96 -.2*3.6, col = 'green')
-invcdf(.1)
+keep <-c()
 
-quadform(3.6/2, .96-.2*3.6, -(3.6/2*(.15)^2 +.96*.15) -.2*3.6*(.15)-.0385)
+for(n in 1:100){
+	currentu <-runif(1,0,1)
+	currentx<-runif(1,0,1)
+	newcurrentx<-invcdf(currentx)
+	currentgx<-f(newcurrentx)
+	currenthx<-envelope(newcurrentx)
+	if (currenu <(currentgx/currenthx)){
+		keep <-c(keep, currentx)
+	}
+}
+
+
+
 
